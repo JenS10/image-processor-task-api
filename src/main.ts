55e93e5b6395c1b002713f53e1670c11e8a3de08
Jenny Salas <jen.salas89@gmail.com>
2025-07-05 10:54:25 +0200

    docs: add Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Image processor task API')
+    .setDescription('API for image processing and task tracking')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
