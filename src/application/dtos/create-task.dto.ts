d8d548d90d398d6c8ac7c4aa5fea9a3f8e0ebb0d
Jenny Salas <jen.salas89@gmail.com>
2025-07-07 19:28:54 +0200

    fix(dto): remove path regex validation

--- src/application/dtos/create-task.dto.ts
+++ src/application/dtos/create-task.dto.ts
@@ -5,9 +5,6 @@ export class CreateTaskDto {
   @IsString()
   @IsNotEmpty({ message: 'Path is required' })
   @Length(5, 200)
-  @Matches(/^([a-zA-Z]:)?(\/|\\)[\w\-.\\/]+$/, {
-    message: 'Invalid file path format',
-  })
   @ApiProperty({
     description: 'Path to the image to process',
     example: '/images/myphoto.jpg',
